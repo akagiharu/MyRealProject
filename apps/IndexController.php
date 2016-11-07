33d1eff14e47eb978c5d2390f6508f370506b7c4
akagiharu <nunommonteiro12@gmail.com>
2016-11-07 22:26:30 +0000

    Create IndexController.php

---
+++ apps/IndexController.php
@@ -0,0 +1,24 @@
+<?php
+
+	use \Phalcon\Mvc\Controller;
+
+	class IndexController extends Controller
+	{
+
+		public $testing;
+		public $url;
+
+		public function indexAction()
+		{
+			$this->view->get_title = $this->get_title();
+			$this->view->getActionName();
+
+		}
+
+		public function get_title()
+		{
+			return "DaizGroup - Only for you with great stuff";
+		}
+	}
+
+?>
