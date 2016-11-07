c22f426bb3be10b80d5694a96a41b528dd499051
akagiharu <nunommonteiro12@gmail.com>
2016-11-07 22:33:58 +0000

    Create pt-PT.php

---
+++ messages/pt-PT.php
@@ -0,0 +1,25 @@
+<?php
+
+$messages = array(
+
+  "AboutDesc" = "DaizGroup foi pensado e criado no dia 30 de Outubro de 2016 em Braga. O Presidente pensou sobre isto para arranjar uma maneira mais fácil para as pessoas comprarem na loja com uma experiência bastante segura nas gateways. Ele pensou que não seria pior se a nossa empresa fizesse pagamentos da loja de retail com as mais seguras métodos de pagamento. Também, estamos a desenvolver um método de pagamento, fácil de usar e seguro, e que só funciona neste website. A nossa empresa vende tudo, desde livros a tudo sobre tecnologia. Este website é também feito com o coração do nosso presidente e equipa para os nossos clientes.",
+  "Do-you-shop-a-lot" = "Gostas de comprar muito",
+  "Shop-Now" = "Compra Já",
+  "About-Us" = "Sobre Nós",
+  "Partners" = "Parceiros",
+  "Services" = "Serviços",
+  "blockquote" = "Will not be possible if you don't have determination, passion, warrior spirit and never give up. Follow your dreams and wait for the right moment.",
+  "SadPartners" = "Still looking for partners",
+  "OnlineShopping" = "Online Shopping",
+  "DaizGateway" = "Daiz Payment's Gateway",
+  "OnlineShoppingDesc" = "A easy way to shop with a better UI/UX for our clients",
+  "DaizGatewayDesc" = "A better way to do payments through our online shop",
+  "Name" = "Name",
+  "Email" = "Email",
+  "Subject" = "Subject",
+  "Message" = "Message",
+  "Send" = "Send",
+  "Reset" = "Reset"
+);
+
+?>
