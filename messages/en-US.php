c82e61527dc880017fa5b2b7f03920b1ff6a5f2b
akagiharu <nunommonteiro12@gmail.com>
2016-11-07 22:32:16 +0000

    Create en-US.php

---
+++ messages/en-US.php
@@ -0,0 +1,28 @@
+<?php
+
+$messages = array(
+
+  "AboutDesc" => "DaizGroup was thought and createad in 30th October 2016 in Braga. The CEO thought about this to give a easy way to people buy with a better gateway security.
+  He thought it wouldn't be worst if our company just do payments of shopping retailing with secure gateways.
+  Also we developed a gateway payment, easy and secure, and it works on this website. Our company sells everything from books to anything of technology. This website is also done with the heart of our own CEO and his team for our clients.",
+  "Do-you-shop-a-lot" => "Do you shop a lot",
+  "Shop-Now" => "Shop Now",
+  "About-Us" => "About Us",
+  "Partners" => "Partners",
+  "Services" => "Services",
+  "blockquote" => "Will not be possible if you don't have determination, passion, warrior spirit and never give up. Follow your dreams and wait for the right moment.",
+  "SadPartners" => "Still looking for partners",
+  "OnlineShopping" => "Online Shopping",
+  "DaizGateway" => "Daiz Payment's Gateway",
+  "OnlineShoppingDesc" => "A easy way to shop with a better UI/UX for our clients",
+  "DaizGatewayDesc" => "A better way to do payments through our online shop",
+  "Name" => "Name",
+  "Email" => "Email",
+  "Subject" => "Subject",
+  "Message" => "Message",
+  "Send" => "Send",
+  "Reset" => "Reset",
+  "About" => "About"
+);
+
+?>
